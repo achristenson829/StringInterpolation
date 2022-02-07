9daeec01268ece9950c99fe6b8d76daaf385fdd7
achristenson829 <achristenson829@gmail.com>
2022-02-07 03:42:54 -0600

    Initial commit

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine($"{$"{today:MMMM} {today:dd}, {today:yyyy}",40}");
+            Console.WriteLine($"{today:yyyy}.{today:MM}.{today:dd}");
+            Console.WriteLine($"Day {today:dd} of {today:MMMM}, {today:yyyy}");
+            Console.WriteLine($"Year: {today:yyyy}, Month:{today:MM}, Day:{today:dd}");
+            Console.WriteLine($"{today,10:dddd}");
+            Console.WriteLine($"{today,10:hh:mm tt}{today,10:dddd}");
+            Console.WriteLine($"h:{today:hh}, m:{today:mm}, s:{today:ss}");
+            Console.WriteLine($"{today:yyyy}.{today:MM}.{today:dd}.{today:hh}.{today:mm}.{today:ss}");
+            var pi = Math.PI;
+            Console.WriteLine($"{pi:C}");
+            Console.WriteLine($"{pi,10:F3}");
+        }
+    }
+}
